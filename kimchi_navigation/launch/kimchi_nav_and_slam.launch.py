44b464a36b41674a4094be21b720facc576c41da
Lola Segura <lsegura@thirdwave.ai>
2025-06-25 18:38:39 -0300

    Readds import lost in merge

--- kimchi_navigation/launch/kimchi_nav_and_slam.launch.py
+++ kimchi_navigation/launch/kimchi_nav_and_slam.launch.py
@@ -2,7 +2,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess, TimerAction
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess, TimerAction, LogInfo
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
