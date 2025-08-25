b9918e938c783c774ca73e78962c19d3b7b50e36
Ari Lowy <arilow@ekumenlabs.com>
2025-08-24 22:07:58 -0300

    Autoreview

--- kimchi_navigation/launch/kimchi_nav_and_slam.launch.py
+++ kimchi_navigation/launch/kimchi_nav_and_slam.launch.py
@@ -58,8 +58,6 @@ def generate_launch_description():
         launch_arguments={
             'use_sim_time': LaunchConfiguration("use_sim_time"),
             'slams_param_file': os.path.join(pkg_slam_toolbox, 'config', 'mapper_params_online_async.yaml'),
-            # 'autostart': 'false', ??????????????????????????????????????
-
         }.items(),
     )
 
@@ -73,7 +71,7 @@ def generate_launch_description():
         executable='global_localization',
         name='global_localization',
         output='screen',
-        # arguments=['--ros-args', '--log-level', 'DEBUG'],
+        arguments=['--ros-args', '--log-level', 'INFO'],
         parameters=[{
             'position_covariance_threshold': 0.5,
             'orientation_covariance_threshold': 0.3,
